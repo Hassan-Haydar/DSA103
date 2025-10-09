167da181b8e43371d7bc42d36c26045631ef1daf
Hassan Haydar <hassan_haydar_@outlook.com>
2025-10-09 22:57:00 +0200

    deleted

--- python-chemistry-intro/src/dsa103/lecture 7/wine_ex.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_wine
-import pandas as pd
-
-wine = load_wine()
-wine_df = pd.DataFrame(wine.data, columns=wine.feature_names)
-wine_df["target"] = wine.target
-# print(wine_df.head())
-
-wine.head()
